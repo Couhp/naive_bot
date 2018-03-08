b7ebf586b74ee7a38988629b6136abfaa3312bc3
phuoc <phuocluu96@gmail.com>
2018-03-08 16:07:12 +0000

    ok

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
-from flask import request
+from flask import request, json
+from pymessenger.bot import Bot
+import requests
 app = Flask(__name__)
 
-@app.route('/')
+ACCESS = "EAAB8i2fNMN8BAHNuHYu8Rs8x1qszrEnlpPgamXlwkFwsBCrao8lKZBa90I29F6XblbhZBeA9xCxqZCms0ZCqZCcIxb1lpc2SP1ke8b6J3JyQnxms4ZAkZAREY5LgWLHDieUeqPWigZCEZBSWThx8AFVxfoANaeAFL3gWLfv7dJAY6M9jXbt5oz9zT"
+bot = Bot(ACCESS)
+
+
+
+@app.route('/', methods=['GET'])
 def hello_world():
     user = request.args.get('hub.challenge')
     return user
+
+
+@app.route('/', methods=['POST'])
+def chat():
+    id = json.loads(request.data)["entry"][0]["messaging"][0]["sender"]["id"]
+    print (id)
+    bot.send_text_message(id, "response")
+    
+    
+    return "ok"
 
 
 app.run(port=8080, host="0.0.0.0")
