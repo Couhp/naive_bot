c669ad49dc2ac54ddf4b3454cb130278cb08ba55
Couhp <phuocluu96@gmail.com>
2018-03-08 22:07:43 +0700

    ok

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    user = request.args.get('hub.challenge')
+    return user
 
 
 app.run(port=8080, host="0.0.0.0")
